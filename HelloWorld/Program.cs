19f75605a72617c94d12fecc309996bb17ed561d
Yufarinson <yufarinxithodiaz@gmail.com>
2025-10-18 13:10:21 -0500

    Change hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Name = "Yufarinson";
+            Console.WriteLine($"Hello, {Name}!");
+            Console.WriteLine();
+        }
+    }
+}
