1b723e8519a82cf7af4193ad8b4617bbdaef0ccf
Yufarinson <yufarinxithodiaz@gmail.com>
2025-10-18 13:47:53 -0500

    Add new code for age calculation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,34 @@
     {
         static void Main(string[] args)
         {
-            const string Name = "Yufarinson";
-            Console.WriteLine($"Hello, {Name}!");
-            Console.WriteLine();
+            DateOnly  dateConverted = new DateOnly();
+            Console.WriteLine("Hello, Bienvenido al calculador de años!");
+            Console.WriteLine("Escribe tu nombre: ");
+            string nameInput = Console.ReadLine();
+            Console.WriteLine($"Un gusto conocerte {nameInput}!");
+            Console.WriteLine("Escribe tu fecha de nacimiento en formato (dd-MM-YYYY): ");
+            string birthDateInput = Console.ReadLine();
+            bool isDateValid = DateOnly.TryParse(birthDateInput, out dateConverted);
+            if (isDateValid == false) Console.WriteLine($"Tu fecha de nacimiento es inválida, usted nos envió dsato erróneo: {birthDateInput}");
+            var person = new Person
+            {
+                Name = nameInput,
+                BirthDate = dateConverted,
+                Age = DateTime.Now.Year - dateConverted.Year
+            };
+
+            Console.WriteLine($"Tu nombre: {person.Name}");
+            Console.WriteLine($"Tu fecha de nacimiento es: {person.BirthDate}");
+            Console.WriteLine($"Tu edad es: {person.Age} años");
+
+            Console.ReadLine();
         }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateOnly BirthDate { get; set; }
     }
 }
